fe5137bfa32ad13967ee0730b9d6586b82018efd
Admin <support@unlimitpotential.com>
2023-05-24 17:31:22 -0400

    g

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -7,9 +7,8 @@ import s from './Navbar.module.css'
 import NavbarRoot from './NavbarRoot'
 import { Logo, Container } from '@components/ui'
 import { Searchbar, UserNav } from '@components/common'
-import  navigation  from '../../../pages/api/categories'; // Update the path if needed
 
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState } from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import {
   Bars3Icon,
@@ -18,6 +17,138 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 
+const navigation = {
+  categories: [
+    {
+      id: 'women',
+      name: 'Womens',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: 'https://nb4fshop.com/search/womens',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+          imageAlt:
+            'Models sitting back to back, wearing Basic Tee in black and bone.',
+        },
+        {
+          name: 'Basic Tees',
+          href: 'https://nb4fshop.com/search/womens',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+          imageAlt:
+            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+        },
+      ],
+      sections: [
+        {
+          id: 'clothing',
+          name: 'Clothing',
+          items: [
+            { name: 'Tops', href: '#' },
+            { name: 'Dresses', href: '#' },
+            { name: 'Pants', href: '#' },
+            { name: 'Denim', href: '#' },
+            { name: 'Sweaters', href: '#' },
+            { name: 'T-Shirts', href: '#' },
+            { name: 'Jackets', href: '#' },
+            { name: 'Activewear', href: '#' },
+            { name: 'Browse All', href: 'https://nb4fshop.com/search/womens' },
+          ],
+        },
+        {
+          id: 'accessories',
+          name: 'Accessories',
+          items: [
+            { name: 'Watches', href: '#' },
+            { name: 'Wallets', href: '#' },
+            { name: 'Bags', href: '#' },
+            { name: 'Sunglasses', href: '#' },
+            { name: 'Hats', href: '#' },
+            { name: 'Belts', href: '#' },
+          ],
+        },
+        {
+          id: 'brands',
+          name: 'Brands',
+          items: [
+            { name: 'Full Nelson', href: '#' },
+            { name: 'My Way', href: '#' },
+            { name: 'Re-Arranged', href: '#' },
+            { name: 'Counterfeit', href: '#' },
+            { name: 'Significant Other', href: '#' },
+          ],
+        },
+      ],
+    },
+    {
+      id: 'men',
+      name: 'Mens',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: 'https://nb4fshop.com/search/mens',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
+          imageAlt:
+            'Drawstring top with elastic loop closure and textured interior padding.',
+        },
+        {
+          name: 'Artwork Tees',
+          href: 'https://nb4fshop.com/search/mens',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+          imageAlt:
+            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+        },
+      ],
+      sections: [
+        {
+          id: 'clothing',
+          name: 'Clothing',
+          items: [
+            { name: 'Tops', href: '#' },
+            { name: 'Pants', href: '#' },
+            { name: 'Sweaters', href: '#' },
+            { name: 'T-Shirts', href: '#' },
+            { name: 'Jackets', href: '#' },
+            { name: 'Activewear', href: '#' },
+            { name: 'Browse All', href: 'https://nb4fshop.com/search/mens' },
+          ],
+        },
+        {
+          id: 'accessories',
+          name: 'Accessories',
+          items: [
+            { name: 'Watches', href: '#' },
+            { name: 'Wallets', href: '#' },
+            { name: 'Bags', href: '#' },
+            { name: 'Sunglasses', href: '#' },
+            { name: 'Hats', href: '#' },
+            { name: 'Belts', href: '#' },
+          ],
+        },
+        {
+          id: 'brands',
+          name: 'Brands',
+          items: [
+            { name: 'Re-Arranged', href: '#' },
+            { name: 'Counterfeit', href: '#' },
+            { name: 'Full Nelson', href: '#' },
+            { name: 'My Way', href: '#' },
+          ],
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: 'Big & Tall', href: 'https://nb4fshop.com/search/bt' },
+    { name: 'Dresses', href: 'https://nb4fshop.com/search/womens-dresses' },
+    { name: 'Rompers', href: 'https://nb4fshop.com/search/rompers' },
+    { name: 'Plus Size', href: 'https://nb4fshop.com/search/plus-size' },
+    { name: 'Jeans', href: 'https://nb4fshop.com/search/denim-jeans-set' },
+  ],
+}
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
@@ -32,17 +163,6 @@ interface NavbarProps {
 }
 const Navbar: FC<NavbarProps> = ({ links }) => {
   const [open, setOpen] = useState<boolean>(false)
-  const [categories, setCategories] = useState([]);
-  const [pages, setPages] = useState([]);
-
-  useEffect(() => {
-    fetch('/api/categories')
-      .then((res) => res.json())
-      .then((data) => {
-        setCategories(data.categories);
-        setPages(data.pages);
-      });
-  }, []);
 
   // rest of the component code
   return (
