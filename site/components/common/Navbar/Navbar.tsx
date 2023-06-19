dc6510ddfef5998b0ba048b00d292ebabc083e6a
Admin <support@unlimitpotential.com>
2023-06-19 16:19:44 -0400

    s

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -138,7 +138,8 @@ const navigation = {
   ],
   pages: [
 
-    
+    { name: 'All Products', href: 'https://nb4fshop.com/search/' },
+
     { name: 'Big & Tall', href: 'https://nb4fshop.com/search/bt' },
     { name: 'Dresses', href: 'https://nb4fshop.com/search/womens-dresses' },
     { name: 'Rompers', href: 'https://nb4fshop.com/search/rompers' },
